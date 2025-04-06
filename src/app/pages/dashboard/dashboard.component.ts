97a32fa0af19c607f24c6b86eff7a84892e3235b
juan david montenegro <davidmontenegro-b@hotmail.com>
2025-04-06 07:51:50 -0500

    cambios

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,20 +1,18 @@
-import { ChangeDetectorRef, Component, inject, OnDestroy } from '@angular/core';
-import { MediaMatcher } from '@angular/cdk/layout';
+import {ChangeDetectorRef, Component, inject, OnDestroy } from '@angular/core';
+import {MediaMatcher } from '@angular/cdk/layout';
 import {MatListModule} from '@angular/material/list';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
-import { RouterLink, RouterOutlet } from '@angular/router';
-import { NavComponent } from "../nav/nav.component";
-import { AuthService } from '../../auth/auth-services/auth.service';
+import {RouterLink,RouterOutlet } from '@angular/router';
+import {NavComponent } from "../nav/nav.component";
+import {AuthService } from '../../auth/auth-services/auth.service';
 import {MatSelectModule} from '@angular/material/select';
 import {MatMenuModule} from '@angular/material/menu';
 
-
-const materialModules = [MatSidenavModule ,MatButtonModule, MatToolbarModule,MatButtonModule,MatMenuModule,
-   MatIconModule, MatListModule , MatToolbarModule , MatSelectModule];
-
+const materialModules = [MatSidenavModule,MatButtonModule,MatToolbarModule,MatButtonModule,
+  MatMenuModule,MatIconModule,MatListModule,MatToolbarModule,MatSelectModule];
 @Component({
   selector: 'app-dashboard',
   standalone: true,
@@ -26,7 +24,6 @@ export class DashboardComponent implements OnDestroy {
   mobileQuery: MediaQueryList;
   isMenuOpen: boolean = true;
   private _mobileQueryListener: () => void;
-
   constructor( public authService : AuthService) {
     const changeDetectorRef = inject(ChangeDetectorRef);
     const media = inject(MediaMatcher);
@@ -35,7 +32,6 @@ export class DashboardComponent implements OnDestroy {
     this.mobileQuery.addEventListener('change', this._mobileQueryListener);
   }
   ngOnDestroy(): void {
-    //this.mobileQuery.removeListener(this._mobileQueryListener);
     this.mobileQuery.removeEventListener('change', this._mobileQueryListener);
   }
 }
